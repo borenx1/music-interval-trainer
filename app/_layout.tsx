b9568395c87c46478e8dbcc28fe3ab19842a9f49
Boren Xue <53597966+borenx1@users.noreply.github.com>
2024-08-25 18:54:35 +1200

    Add metadata for web version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from 'expo-router';
+import Head from 'expo-router/head';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Head>
+        <title>Music Interval Trainer</title>
+      </Head>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: 'ABC', headerShown: false }} />
+      </Stack>
+    </>
   );
 }
